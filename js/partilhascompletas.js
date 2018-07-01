f46efea76e1bcd334f647ded7baf12882f7bea2b
Fernando Zendron <fernandozendron@Fernandos-MacBook-Pro.local>
2018-07-01 13:44:38 -0300

    search

---
+++ js/partilhascompletas.js
@@ -0,0 +1,263 @@
+var doc1 = {
+	"id":"c001",
+	"title":"A cura",
+	"description":"A condição natural da humanidade e sua necessidade de cura. Nosso papel nessa circunstância",
+	"keywords":"1988"
+};
+
+var doc2 = {
+	"id":"c002",
+	"title":"O caminho da cura",
+	"description":"A enfermidade existe só no nível material; à medida que contatarmos níveis superiores de energia, obteremos a cura",
+	"keywords":"1988"
+};
+
+var doc3 = {
+	"id":"c003",
+	"title":"Rumo à cura",
+	"description":"Muitas vezes as enfermidades são um recurso da natureza para equilibrar o ser. Uma reflexão sobre o seu modo de vida e uma volta ao eu interno lhe possibilitarão encontrar o caminho da cura",
+	"keywords":"1988"
+};
+
+var doc4 = {
+	"id":"c004",
+	"title":"Rumo à cura",
+	"description":"O ser caminha para a cura quando desenvolve o autoconhecimento e a intuição. Para isso deve desapegar-se, superar os desejos e a expectativa por resultados, entregar-se ao serviço ao Plano Evolutivo",
+	"keywords":"1988"
+};
+
+var doc5 = {
+	"id":"c005",
+	"title":"Curar é simples",
+	"description":"Curar é ajudar o outro a tornar-se cada vez mais consciente dos níveis superiores do próprio ser",
+	"keywords":"1988"
+};
+
+var doc6 = {
+	"id":"c006",
+	"title":"Três processos de cura",
+	"description":"Como nos tornarmos transformadores",
+	"keywords":"1988"
+};
+
+var doc7 = {
+	"id":"c007",
+	"title":"O corpo físico na cura",
+	"description":"A busca além das nossas forças",
+	"keywords":"1988"
+};
+
+var doc8 = {
+	"id":"c008",
+	"title":"Cura dos apegos",
+	"description":"O apego surge da falta de contato com o lado espiritual das coisas e se dissolve quando entramos no próprio coração",
+	"keywords":"1983"
+};
+
+var doc9 = {
+	"id":"c009",
+	"title":"A medicina espiritual e as provas sutis do caminho",
+	"description":"O tratamento espiritual como oportunidade de prova, de aprendizagem, de transformação e de ampliação da consciência para si e para os que estão próximos",
+	"keywords":"2000"
+};
+
+var doc10 = {
+	"id":"c010",
+	"title":"A função do sofrimento",
+	"description":"Compreender o sofrimento como meio de equilíbrio e elevação da consciência leva o ser à cura espiritual",
+	"keywords":"1988"
+};
+
+var doc11 = {
+	"id":"c011",
+	"title":"Instrumentos de cura em Figueira",
+	"description":"Os instrumentos de cura só podem atuar quando existe em nós disposição profunda e verdadeira de transformar-nos",
+	"keywords":"2001"
+};
+
+var doc12 = {
+	"id":"c012",
+	"title":"Contatos com os irmãos do Espaço e a cura em Figueira",
+	"description":"No contato com os irmãos do Cosmos encontram-se chaves para a evolução. Quem recebe atendimentos deve colaborar com sua cura, buscando contatar planos superiores",
+	"keywords":"2000"
+};
+
+var doc13 = {
+	"id":"c013",
+	"title":"Iberah, Lyz, Mirna Jad e a graça nos processos de cura",
+	"description":"A doença vista como ato de justiça cármica, e a energia da Mãe Universal como cura pela ação da Graça",
+	"keywords":"2001"
+};
+
+var doc14 = {
+	"id":"c014",
+	"title":"A cura do corpo mental e o trato com pacientes terminais",
+	"description":"O verdadeiro trabalho é o de proporcionar condições para que o corpo mental humano ingresse em outro estado, acima do nível comum",
+	"keywords":"2001"
+};
+
+var doc15 = {
+	"id":"c015",
+	"title":"A cura cósmica",
+	"description":"A cura cósmica é a unificação do ser inteiro com sua vontade superior e suprafísica. Dá-se em outro tempo e em outro espaço",
+	"keywords":"1989"
+};
+
+var doc16 = {
+	"id":"c016",
+	"title":"A formação de curadores",
+	"description":"Quando alguém se torna seu próprio curador, integra-se na vida cósmica. Isso é possível no momento atual. Vem do voltar-se para o centro mais profundo da consciência",
+	"keywords":"1989"
+};
+
+var doc17 = {
+	"id":"c017",
+	"title":"O trabalho do terapeuta e o trabalho do curador",
+	"description":"Ser instrumento do espírito e ajudar o outro a se responsabilizar pelo próprio processo de cura / O preparo para a cura cósmica",
+	"keywords":"2002"
+};
+
+var doc18 = {
+	"id":"c018",
+	"title":"Curadores, curas e cirurgias espirituais",
+	"description":"A cura só pode suceder verdadeiramente quando os que a almejam buscam a união com a pura essência do próprio ser. O êxito das cirurgias espirituais e de outros processos depende do trabalho interior de quem se submete a eles",
+	"keywords":"2008"
+};
+
+var doc19 = {
+	"id":"c019",
+	"title":"Curadores e ajuda das naves",
+	"description":"Enquanto a humanidade vai preparando sua própria destruição, a energia de centros planetários como Aurora se organiza para no momento certo regenerar o solo e a água do planeta, curar as nossas células e as dos animais",
+	"keywords":"2005"
+};
+
+var doc20 = {
+	"id":"c020",
+	"title":"Aos médicos e terapeutas",
+	"description":"Indicações aos que buscam ser curadores autênticos. A importância de médicos e terapeutas cuidarem da própria cura, renovarem-se no centro do próprio ser, estarem disponíveis para contatos internos",
+	"keywords":"2002"
+};
+
+var doc21 = {
+	"id":"c021",
+	"title":"Doenças especiais e perigos extremos",
+	"description":"Como prover proteção e segurança em situações anormais e tratar doenças especiais",
+	"keywords":"2001"
+};
+
+var doc22 = {
+	"id":"c022",
+	"title":"Câncer, sistema nervoso e glândulas",
+	"description":"Valores, potenciais e metas da prática médica que conta com a intuição, sintoniza com os planos internos da vida e transcende os cuidados com o corpo material",
+	"keywords":"2004"
+};
+
+var doc23 = {
+	"id":"c023",
+	"title":"Os medos",
+	"description":"Só ficaremos fortes quando deixarmos de temer a Realidade. Seremos curados quando soubermos estar diante da Verdade",
+	"keywords":"2002"
+};
+
+var doc24 = {
+	"id":"c024",
+	"title":"Diagnóstico e depressão",
+	"description":"Meios não convencionais de diagnosticar: clarividência, conhecimento anímico e transferência oculta. Causas energéticas da depressão hoje",
+	"keywords":"2004"
+};
+
+var doc25 = {
+	"id":"c025",
+	"title":"A cura pelos sons, aromas e vapores",
+	"description":"Certos procedimentos de cura sutis, aplicados com amor, podem tocar o coração e a alma e ativar o tão necessário sentimento de devoção",
+	"keywords":"2002"
+};
+
+var doc26 = {
+	"id":"c026",
+	"title":"As cores na cura e a formação da nossa nave",
+	"description":"Tornar-se receptivo à atuação das cores. A formação do corpo de luz por meio do serviço",
+	"keywords":"2002"
+};
+
+var doc27 = {
+	"id":"c027",
+	"title":"Uma luz na medicina",
+	"description":"As energias de cura estão em níveis profundos do ser, e sua atuação vai muito além dos tratamentos convencionais e do conhecimento médico",
+	"keywords":"2002"
+};
+
+var doc28 = {
+	"id":"c028",
+	"title":"Medicina e a força de vida universal",
+	"description":"É preciso reencontrar a essência da Medicina. O que cura realmente não são os medicamentos, as terapias nem os médicos, mas sim uma força de vida onipresente",
+	"keywords":"2004"
+};
+
+var doc29 = {
+	"id":"c029",
+	"title":"Medicina e a força de vida universal",
+	"description":"A união da Medicina com a Psicologia, a Astrologia e a Astronomia no trabalho de cura. Essas ciências estão para ressurgir em novas bases, e por meio delas podemos aprender a reconhecer o cosmos na Terra",
+	"keywords":"2004"
+};
+
+var doc30 = {
+	"id":"c030",
+	"title":"O caminho da verdadeira cura",
+	"description":"Estudo com Trigueirinho no Encontro de Formação Básica para Acolhimento e Atendimento da Casa Luz da Colina",
+	"keywords":"2007"
+};
+
+var doc31 = {
+	"id":"c031",
+	"title":"Processos futuros de cura",
+	"description":"A universalidade é a nota que o Núcleo Sohin deve cultivar para possibilitar a aproximação das raças cósmicas e a manifestação de novos meios de cura",
+	"keywords":"2004"
+};
+
+var doc32 = {
+	"id":"c032",
+	"title":"O despertar para a cura universal",
+	"description":"A cura existente nos Centros Planetários Considerações sobre mensagem de Cristo Jesus de 5 de setembro de 2013 O verdadeiro curador é o que cura através do Amor crístico Restabelecer códigos de vida que se perderam Chamado ao despertar de novos apóstolos de Cristo Orar com o coração para chegar ao princípio da cura",
+	"keywords":"2013"
+};
+
+var idx = elasticlunr(function () {
+		this.addField('title');
+	this.addField('description');
+	this.addField('keywords');
+	this.setRef('id');
+});
+
+idx.addDoc(doc1);
+idx.addDoc(doc2);
+idx.addDoc(doc3);
+idx.addDoc(doc4);
+idx.addDoc(doc5);
+idx.addDoc(doc6);
+idx.addDoc(doc7);
+idx.addDoc(doc8);
+idx.addDoc(doc9);
+idx.addDoc(doc10);
+idx.addDoc(doc11);
+idx.addDoc(doc12);
+idx.addDoc(doc13);
+idx.addDoc(doc14);
+idx.addDoc(doc15);
+idx.addDoc(doc16);
+idx.addDoc(doc17);
+idx.addDoc(doc18);
+idx.addDoc(doc19);
+idx.addDoc(doc20);
+idx.addDoc(doc21);
+idx.addDoc(doc22);
+idx.addDoc(doc23);
+idx.addDoc(doc24);
+idx.addDoc(doc25);
+idx.addDoc(doc26);
+idx.addDoc(doc27);
+idx.addDoc(doc28);
+idx.addDoc(doc29);
+idx.addDoc(doc30);
+idx.addDoc(doc31);
+idx.addDoc(doc32);
